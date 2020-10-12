8a22e5efdc774c2f52efba44a6bebcae84eabe29
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2020-10-13 00:34:14 +0200

    poprawka zad 8

--- 2020-10-12 - zestaw_01/zad_08.py
+++ 2020-10-12 - zestaw_01/zad_08.py
@@ -1,14 +1,19 @@
-given_num = int(input("Podaj liczbę: "))
+given_num = 2 # Nie udowodniono istnienia nieparzystych liczb doskonałych
 
-div_sum = 0
-i = 1
+while given_num < 1000000:
+    div_sum = 0
+    i = 1
 
-while i <= given_num // 2:
-    if given_num % i == 0:
-        div_sum += i
-    i += 1
+    while i <= given_num // 2:
+        if given_num % i == 0:
+            div_sum += i
+        i += 1
 
-if given_num == div_sum:
-    print("Jest liczbą doskonałą")
-else:
-    print("Nie jest liczbą doskonałą")
+    if given_num == div_sum:
+        print(given_num)
+
+    given_num += 2 # Nie udowodniono istnienia nieparzystych liczb doskonałych
+
+    if given_num == 8130:
+        print("Wszystkie znalezione, dalej już tylko czekanie aż doliczy do miliona... Polecam Ctrl+C tbh")
+        # Na prawdę nic tam dalej nie ma, następna liczba doskonała to 33 550 336
