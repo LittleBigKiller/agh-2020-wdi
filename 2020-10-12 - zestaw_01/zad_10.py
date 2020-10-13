b7ebbe5629ca9e574cf6d9d6ae2bf26e47255b0c
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2020-10-13 02:48:04 +0200

    Uproszczenie i sprawienie, że serio działa

--- 2020-10-12 - zestaw_01/zad_10.py
+++ 2020-10-12 - zestaw_01/zad_10.py
@@ -2,37 +2,10 @@ given_num_0 = int(input("Podaj pierwszą liczbę: "))
 given_num_1 = int(input("Podaj drugą liczbę: "))
 given_num_2 = int(input("Podaj trzecią liczbę: "))
 
-def get_divlist(num):
-    temp_list = []
+def nwd(a, b):
+    if b > 0:
+        return nwd(b, a%b)
+    else:
+        return a
 
-    i = 1
-
-    while i * i <= num:
-        if num % i == 0:
-            temp_list.append(i)
-            if num // i != i:
-                temp_list.append(num // i)
-        i += 1
-
-    temp_list.sort()
-
-    return temp_list
-
-divs_0 = get_divlist(given_num_0)
-divs_1 = get_divlist(given_num_1)
-divs_2 = get_divlist(given_num_2)
-
-# print(divs_0)
-# print(divs_1)
-# print(divs_2)
-
-common_divs = list(set(divs_0) & set(divs_1) & set(divs_2))
-
-# print(common_divs)
-
-max_common_div = 1
-
-for i in common_divs:
-    max_common_div *= i
-
-print(max_common_div)
+print(nwd(nwd(given_num_0, given_num_1), given_num_2))
--- 2020-10-12 - zestaw_01/zad_11.py
+++ 2020-10-12 - zestaw_01/zad_11.py
@@ -3,38 +3,14 @@ given_num_1 = int(input("Podaj drugą liczbę: "))
 given_num_2 = int(input("Podaj trzecią liczbę: "))
 
 
-def get_divlist(num):
-    temp_list = []
-
-    i = 1
-
-    while i * i <= num:
-        if num % i == 0:
-            temp_list.append(i)
-            if num // i != i:
-                temp_list.append(num // i)
-        i += 1
-
-    temp_list.sort()
-
-    return temp_list
+def nwd(a, b):
+    if b > 0:
+        return nwd(b, a % b)
+    else:
+        return a
 
 
-divs_0 = get_divlist(given_num_0)
-divs_1 = get_divlist(given_num_1)
-divs_2 = get_divlist(given_num_2)
+def nww(a, b):
+    return a * b // nwd(a, b)
 
-# print(divs_0)
-# print(divs_1)
-# print(divs_2)
-
-common_multi = list(set(divs_0).union(set(divs_1).union(set(divs_2))))
-
-# print(common_multi)
-
-min_common_multi = 1
-
-for i in common_multi:
-    min_common_multi *= i
-
-print(min_common_multi)
+print(nww(nww(given_num_0, given_num_1), given_num_2))
