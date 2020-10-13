6c4dcd27d579f3957e6cf5fafa8d8fe777628c61
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2020-10-13 02:47:43 +0200

    Poprawienie tekstu zwracanego

--- 2020-10-12 - zestaw_01/zad_06.py
+++ 2020-10-12 - zestaw_01/zad_06.py
@@ -1,14 +1,14 @@
 given_num = int(input("Podaj liczbę: "))
 
 if given_num % 2 == 0:
-    print("Nie jest")
+    print("Nie jest pierwsza")
     quit()
 else:
     half = given_num // 2
     i = 3
     while (i < half):
         if given_num % i == 0:
-            print("Nie jest")
+            print("Nie jest pierwsza")
             quit()
         i += 2
-    print("Jest")
+    print("Jest pierwsza")
